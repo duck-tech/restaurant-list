ae7ae4ade800a1ecd4bfe03b1c64fa2a2fd5113f
tech-kelly <babymis0628.mg10@nycu.edu.tw>
2022-02-26 21:35:08 +0800

    feat: add detail page

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express()
 const exphbs = require('express-handlebars')
 // require files
 const restaurantList = require('./restaurant.json')
-const restaurant = require('./models/restaurant')
+const Restaurant = require('./models/restaurant')
 const mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost/restaurant-list')
 
@@ -29,7 +29,7 @@ const port = 3000
 
 // route setting
 app.get('/',(req,res) => {
-  restaurant.find()
+  Restaurant.find()
             .lean()
             .then(restaurants => res.render('index',{restaurants}))
             .catch(error => console.log(error))
@@ -40,17 +40,20 @@ app.get('/restaurants/new' , (req,res) => {
 })
 
 app.post('/restaurants', (req,res) => {
-  restaurant.create(req.body)
+  Restaurant.create(req.body)
             .then(() => res.redirect('/'))
             .catch(error => console.log(error))
 })
 
-
 app.get('/restaurants/:id', (req,res) => {
-  console.log(req.params)
-  const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.id)
-  res.render('show', {restaurant:restaurant})
+  const id = req.params.id
+  return Restaurant.findById(id)
+                  .lean()
+                  .then((restaurant) => res.render('show', {restaurant}))
+                  .catch(error => console.log(error))
 })
+
+
 app.get('/search',(req,res) => {
   const keyword = req.query.keyword.trim()
   const restaurants = restaurantList.results.filter(restaurant => {return restaurant.name.toLowerCase().includes(keyword.toLowerCase()) || restaurant.category.includes(keyword)})
