f8818bfac278dc0816b44223696371dd4b43f35c
tech-kelly <babymis0628.mg10@nycu.edu.tw>
2022-02-26 15:43:39 +0800

    feat: project init

---
+++ app.js
@@ -0,0 +1,36 @@
+// include express from node_modules
+const express = require('express')
+const app = express()
+// require express-handlebars here
+const exphbs = require('express-handlebars')
+// require files
+const restaurantList = require('./restaurant.json')
+
+// setting template engine
+app.engine('handlebars',exphbs.engine({defaultLayout:'main'}))
+app.set('view engine', 'handlebars')
+// setting static files 
+app.use(express.static('public'))
+
+// define server releated variables 
+const port = 3000
+
+// route setting
+app.get('/',(req,res) => {
+  res.render('index',{restaurants: restaurantList.results})
+})
+app.get('/restaurants/:id', (req,res) => {
+  console.log(req.params)
+  const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.id)
+  res.render('show', {restaurant:restaurant})
+})
+app.get('/search',(req,res) => {
+  const keyword = req.query.keyword.trim()
+  const restaurants = restaurantList.results.filter(restaurant => {return restaurant.name.toLowerCase().includes(keyword.toLowerCase()) || restaurant.category.includes(keyword)})
+  res.render('index',{restaurants:restaurants, keyword:keyword})
+})
+
+// start and listen server
+app.listen(port, () => {
+  console.log(`Express is running on http://localhost:${port}`)
+})
