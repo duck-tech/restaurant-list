acec0c4c3295f865408dec3babb6efcdd99c0907
tech-kelly <babymis0628.mg10@nycu.edu.tw>
2022-02-28 00:09:06 +0800

    feat: add sort

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -12,8 +12,34 @@ router.get('/',(req,res) => {
 
 router.get('/search',(req,res) => {
   const keyword = req.query.keyword.trim()
-  const restaurants = restaurantList.results.filter(restaurant => {return restaurant.name.toLowerCase().includes(keyword.toLowerCase()) || restaurant.category.includes(keyword)})
-  res.render('index',{restaurants:restaurants, keyword:keyword})
+  console.log(req.query.sort)
+  const sortCriteria = req.query.sort
+  const sortMethod = {}
+
+  switch (sortCriteria) {
+    case 'asc':
+      sortMethod['name'] = 'asc'
+      break
+    case 'desc':
+      sortMethod['name'] = 'desc'
+      break
+    case 'category':
+      sortMethod['category'] = 'asc'
+      break
+    case 'location':
+      sortMethod['location'] = 'asc'
+      break
+    default:
+      sortMethod['name'] = 'asc'
+  }
+  Restaurant.find()
+          .lean()
+          .sort(sortMethod)
+          .then(restaurants => {
+            restaurants = restaurants.filter(restaurant => {return restaurant.name.toLowerCase().includes(keyword.toLowerCase()) || restaurant.category.includes(keyword)})
+          
+          res.render('index',{restaurants:restaurants, keyword:keyword})
+        })
 })
 
 module.exports = router
